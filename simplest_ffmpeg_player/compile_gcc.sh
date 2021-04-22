a7ccadb6ffd70afabe3bb82b348382a15aa28e81
rwachtfogel <rwachtfogel@synamedia.com>
2021-04-22 14:08:32 -0700

    Initial Cleanup

--- simplest_ffmpeg_player/compile_gcc.sh
+++ simplest_ffmpeg_player/compile_gcc.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 gcc *.cpp -g -o hsplayer  \
--I /usr/local/include -L /usr/local/lib -lSDL2main -lSDL2 -lavformat -lavcodec -lavutil -lswscale
+-I /usr/local/include -L /usr/local/lib -lSDL2main -lSDL2 -lavformat -lavcodec -lavutil -lswscale -pthread
